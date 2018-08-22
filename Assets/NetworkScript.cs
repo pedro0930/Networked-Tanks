2bc174a9c5d77137ea616fac7d515fc38bedf616
pedro0930 <pedro0930@gmail.com>
2018-08-21 21:14:49 -0700

    First Commit

---
+++ Assets/NetworkScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkScript : NetworkManager {
+
+	void OnStartServer(){
+		print ("On start server");
+	}
+	void OnStartClient(){
+		print ("On start client");
+	}
+}
