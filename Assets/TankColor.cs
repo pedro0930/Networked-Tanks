763e5bc4a1b8424bc62293023fcfe3de6a5b7214
pedro0930 <pedro0930@gmail.com>
2018-08-22 21:02:44 -0700

    Tank Color selection is in

---
+++ Assets/TankColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TankColor : NetworkBehaviour {
+
+	[SyncVar]
+	public Color color;
+
+	MeshRenderer[] rends;
+
+	// Use this for initialization
+	void Start () {
+		rends = GetComponentsInChildren<MeshRenderer>();
+		for(int i = 0; i< rends.Length; i++)
+		rends[i].material.color = color;
+	}
+	public void HideTank()
+	{
+		for (int i =0; i< rends.Length; i++)
+			rends [i].material.color = Color.clear;
+	}
+	
+}
---
+++ Assets/TankColor_Hook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Prototype.NetworkLobby{
+	public class TankColor_Hook : Prototype.NetworkLobby.LobbyHook {
+		public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager NetworkManager, GameObject lobbyPlayer, GameObject gamePlayer)
+		{
+			LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+			TankColor tank = gamePlayer.GetComponent<TankColor>();
+
+			tank.color = lobby.playerColor;
+		}
+	}
+}
