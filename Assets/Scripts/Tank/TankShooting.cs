172e3bdb1f1070dfcdaaf047f8af157f9e64374d
v86861062 <v86861062@gmail.com>
2018-08-24 20:36:06 +0800

    Fix: Client can't charge up attack.  "Closes #2"

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -30,7 +30,7 @@ public class TankShooting : NetworkBehaviour
     }
 
 
-    private void Start ()
+    private void Start()
     {
         // The fire axis is based on the player number.
         m_FireButton = "Fire" + m_PlayerNumber;
@@ -40,12 +40,13 @@ public class TankShooting : NetworkBehaviour
     }
 
 
-    private void Update ()
+    private void Update()
     {
-        if (!isLocalPlayer) {
-            return; 
+        if (!isLocalPlayer)
+        {
+            return;
         }
-        
+
         // The slider should have a default value of the minimum launch force.
         m_AimSlider.value = m_MinLaunchForce;
 
@@ -57,10 +58,10 @@ public class TankShooting : NetworkBehaviour
 
             // Set the fired flag so only Fire is only called once.
             m_Fired = true;
-            CmdFire ();
+            CmdFire(m_CurrentLaunchForce);
         }
         // Otherwise, if the fire button has just started being pressed...
-        else if (Input.GetButtonDown (m_FireButton))
+        else if (Input.GetButtonDown(m_FireButton))
         {
             // ... reset the fired flag and reset the launch force.
             m_Fired = false;
@@ -68,10 +69,10 @@ public class TankShooting : NetworkBehaviour
 
             // Change the clip to the charging clip and start it playing.
             m_ShootingAudio.clip = m_ChargingClip;
-            m_ShootingAudio.Play ();
+            m_ShootingAudio.Play();
         }
         // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-        else if (Input.GetButton (m_FireButton) && !m_Fired)
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
         {
             // Increment the launch force and update the slider.
             m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
@@ -79,30 +80,31 @@ public class TankShooting : NetworkBehaviour
             m_AimSlider.value = m_CurrentLaunchForce;
         }
         // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-        else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
         {
             // Set the fired flag so only Fire is only called once.
             m_Fired = true;
             // ... launch the shell.
-            CmdFire ();
+            CmdFire(m_CurrentLaunchForce);
         }
     }
 
     [Command]
-    private void CmdFire ()
+    private void CmdFire(float m_CurrentLaunchForce)
     {
         // Create an instance of the shell and store a reference to it's rigidbody.
         Rigidbody shellInstance =
-            Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
         // Set the shell's velocity to the launch force in the fire position's forward direction.
         shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; ;
 
+        print("m_CurrentLaunchForce: " + m_CurrentLaunchForce);
         NetworkServer.Spawn(shellInstance.gameObject);
 
         // Change the clip to the firing clip and play it.
         m_ShootingAudio.clip = m_FireClip;
-        m_ShootingAudio.Play ();
+        m_ShootingAudio.Play();
 
 
         // Reset the launch force.  This is a precaution in case of missing button events.
